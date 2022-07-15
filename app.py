0e9c02ccd259be931a3a848cea80613739e8a3b5
Daniel <danielmosqueda15@gmail.com>
2022-07-15 02:37:36 -0400

    Started css styling and added extra cases for user errors

--- app.py
+++ app.py
@@ -18,22 +18,6 @@ def show_home_page():
 
     return render_template("home.html")
 
-
-
-# @app.route("/new")
-# def check_home_page():
-
-#     convert_from = request.form["convert_from"]
-#     convert_to = request.form["convert_to"]
-#     amount = request.form["amount"]
-
-#     flash(f"This is the convert_from variable {convert_from}")
-#     flash(f"This is the convert_to variable {convert_to}")
-#     flash(f"This is the amount variable {amount}")
-
-#     return redirect("/test", convert_from=convert_from, convert_to=convert_to, amount=amount)
-
-
 @app.route("/convert", methods = ["GET", "POST"])
 def show_convert_page():
 
@@ -54,17 +38,29 @@ def show_convert_page():
         full_currency_list = b.keys() # Getting all the keys (EUR, USD, GBP etc.)
 
         if convert_from not in full_currency_list: #if the converted_to isn't apart of the list, sends back to original page w/ flash that it's invalid
-            flash(f"{convert_from} is not a valid currency. Please try again")
+            if len(convert_from) == 0: #This checks to see if it was empty
+                flash("Please fill in this field")
+            else: flash(f"{convert_from} is not a valid currency. Please try again")
 
-        if convert_to not in full_currency_list: #if the converted_from isn't apart of the list, sends back to original page w/ flash that it's invalid
-            flash(f"{convert_to} is not a valid currency. Please try again")
+
+        if convert_to not in full_currency_list or len(convert_to) == 0: #if the converted_from isn't apart of the list, sends back to original page w/ flash that it's invalid
+            if len(convert_to) == 0: #This checks to see if it was empty
+                flash("Please fill in this field")
+            else: flash(f"{convert_to} is not a valid currency. Please try again")
 
 
         if amount.isdigit() == False:
-            flash(f"{amount} is not a valid amount. Please enter a valid amount") # Will say it's invalid if it's not a number or if it's negative
-            return redirect("/")
+            # if len(amount) == 0:
+            #     flash("Please fill in this field") 
+            # else: 
+                flash(f"Please enter a valid amount (i.e. No spaces or negative numbers). You have entered '{amount}'.") # Will say it's invalid if it's not a number or if it's negative
+                return redirect("/")
+
+            # Unable to figure out a way to incorporate an error message if the "amount" form was submitted empty like convert_from and convert_to
+
+        d = int(amount)
     
-        conversion = c.convert(convert_from, convert_to, int(amount))
+        conversion = c.convert(convert_from, convert_to, d)
 
         new_amount = round(conversion, 2)
 
@@ -75,22 +71,6 @@ def show_convert_page():
 
         return render_template("convert.html", convert_from=convert_from, convert_to=convert_to, amount=amount, new_amount=new_amount, convert_from_symbol=convert_from_symbol, convert_to_symbol=convert_to_symbol)
 
-
-
-
-@app.route("/test")
-def test_parameters():
-    c = CurrencyRates()
-    b = c.get_rates('USD')
-    b['USD'] = 1
-    res = b.keys()
-    
-    return (str(res))
-
-## Next Step: Find out how to do error messages: Convert from, convert to, and amount
-## -Probably have to do conditional logic: if it doesn't = the key, or for loop to go over each key
-## -Flash Messages???
-## -Create a div or message that's stored in cookie or sessions???
 
 ## Decorate???
 ## At least make the form and button look neater/clean
